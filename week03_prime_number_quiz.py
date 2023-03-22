ec7256ed3336cb37b3150e22213c1837e8843dc3
ag4710 <leun4710@gmail.com>
2023-03-22 21:32:13 +0900

    week03_prime_number_quiz

---
+++ week03_prime_number_quiz.py
@@ -0,0 +1,16 @@
+# week03_prime_number_quiz
+start_no = int(input("Enter starting number : "))
+end_no = int(input("Enter ending number : "))
+is_prime = True
+
+for k in range(start_no,end_no+1):
+    is_prime = True
+    if k < 2:
+        is_prime = False
+    else:
+        for i in range(2, k):
+            if k % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(k, end=" ")
