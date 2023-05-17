1b94a8f9a04254033382e84687d160ef4cab461e
ag4710 <leun4710@gmail.com>
2023-05-17 19:42:37 +0900

    week09 class&object 01

---
+++ week09_class_object03.py
@@ -0,0 +1,15 @@
+class Pokemon:
+    pass
+
+if __name__ == "__main__":
+    pikachu = Pokemon()  # instance creation
+    eevee = Pokemon()  # instance creation
+
+    pikachu.level = 1
+    pikachu.hp = 35
+    pikachu.nemesis = eevee
+
+    print(pikachu.hp)
+    print(pikachu.nemesis)
+    print(pikachu)
+    
