9bbc227c670c1733885bfa1d4fc6b3ae7902f07a
ag4710 <leun4710@gmail.com>
2023-05-24 21:26:34 +0900

    week10 class&object 13

--- week10_class_object05.py
+++ week10_class_object05.py
@@ -9,14 +9,32 @@ class Pokemon:
         self.level = level
         Pokemon.pokemon_count = Pokemon.pokemon_count + 1
 
+    def __add__(self, other):
+        """
+        both pokemon's total hp & level
+        :param other:
+        :return:
+        """
+        return f'total hp : {self.hp + other.hp}, total level : {self.level + other.level}'
+
+
+    def __str__(self):
+        """
+        <__main__.Pokemon object at 0x0000018B7EC3BC40>
+        to
+        I'm a [Pokemon name]!
+        :return:
+        """
+        return  f"I'm a {self.__name}!"
+
     @staticmethod
     def intro():
         print("ISHS Pokemon Game")
+
     @classmethod
     def print_pokemon_count(cls):
         # print(f"pokemon population : {Pokemon.pokemon_count}")
         print(f"pokemon population : {cls.pokemon_count}")
-
 
     @property
     def name(self):
@@ -28,7 +46,6 @@ class Pokemon:
         print("setter executed!")
         self.__name = input_name
 
-
     def info(self):
         print("=============")
         print(f"Name : {self.__name}")
@@ -38,9 +55,13 @@ class Pokemon:
 
 
 if __name__ == "__main__":
-    Pokemon.intro()
+    Pokemon.intro() # static method
     p1 = Pokemon("pikachu", 35, 1)
     p2 = Pokemon("squirtle", 40, 1)
     p3 = Pokemon("charizard", 85, 36)
-    print(Pokemon.pokemon_count)
-    Pokemon.print_pokemon_count()
+    print(Pokemon.pokemon_count)  # class field
+    Pokemon.print_pokemon_count()  # class method
+    print(p1 + p2)  # __add__
+    # __str__
+    print(p3)
+    print(p1)
