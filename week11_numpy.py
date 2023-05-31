47d015aaedf9043a39375699346d60f6f91ccbc0
ag4710 <leun4710@gmail.com>
2023-05-31 20:44:22 +0900

    week11 numpy 06

--- week11_numpy.py
+++ week11_numpy.py
@@ -1,24 +1,23 @@
 import numpy as np
 
-# b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) + np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-# b = np.array(range(1, 11)) + np.array(range(10, 101, 10))
-b = np.arange(1, 11) + np.arange(10, 101, 10)
-print(b, b.shape, b.size, b.ndim)
 d2_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # list style
 print(d2_array[2][0], d2_array[-1][-3], d2_array[2][-3])
 # numpy style
 print(d2_array[2, -3], d2_array[-1, 0])
+d2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print(d2)
 
-scores = np.array([2500000, 2200000, 2300000])
-# scores = scores + 50
-# scores = scores * 3
-scores = scores * 0.026 / 12.0
-print(scores)
 d2_array[0, 0] = 11
 print(d2_array)
+print(d2[::2][::2])  # 첫 슬라이싱 : 0행, 2행 선택 후 /// 두 번째 슬라이싱: 그 중 0행 선택
+print(d2[::2, ::2])  # 행 슬라이싱 : 0행, 2행 선택 /// 열 슬라이싱 : 0열, 2열 선택
 
-mixed = np.array([-9, 'ISHS', False, 2.71])
-print(mixed)
 d2_array[0, 0] = 22.71  # truncation
 print(d2_array)
+print(d2[0:2, 2:])
+print(d2[1], d2[1,:])
+print(d2[:, 2:3])
+print(d2[:, 2])
+print(d2[0:2, 0:2])
+print(d2[1::2, 1::2])
